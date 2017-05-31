6c549df145ba3314509d17a199324538aca582e6
Tobias Lopez <tobias.lopez@opten.ch>
2017-05-31 09:16:24 +0200

    added directive to config & using umbracos confirm prompt

--- src/Opten.Umbraco.Localization.Web.UI/App_Plugins/OPTEN.Localization.Migration/opten.localization.migration.directive.js
+++ src/Opten.Umbraco.Localization.Web.UI/App_Plugins/OPTEN.Localization.Migration/opten.localization.migration.directive.js
@@ -7,8 +7,10 @@
 
 	function propertyMigration(localizationMigrationResource, $route) {
 		var directive = {
-			restrict: "A",
-			link: link,
+			restrict: "E",
+			controller: controller,
+			replace: true,
+			templateUrl: '/App_Plugins/OPTEN.Localization.Migration/directives/propertyMigration.html?v=ASSEMBLY_VERSION',
 			scope: {
 				tab: '=tab',
 				contentType: '=contentType',
@@ -18,25 +20,32 @@
 
 		return directive;
 
-		function link(scope, element, attr) {
-			element.bind('click', function () {
-				if (confirm("Do you want to localize this property?")) {
-					var property = {
-						alias: scope.property.alias,
-						contentTypeAlias: scope.contentType.alias,
-						dataTypeDefinitionId: scope.property.dataTypeId,
-						group: scope.tab.name,
-						localize: true,
-						name: scope.property.label,
-						propertyEditorAlias: scope.property.editor
+		function controller($scope) {
+			$scope.property.localize = false;
 
-					}
+			$scope.togglePrompt = function () {
+				$scope.property.localize = !$scope.property.localize;
+			}
 
-					localizationMigrationResource.postProperty(property).then(function () {
-						$route.reload();
-					});
+			$scope.hidePrompt = function () {
+				$scope.property.localize = false;
+			}
+
+			$scope.localize = function () {
+				var property = {
+					alias: $scope.property.alias,
+					contentTypeAlias: $scope.contentType.alias,
+					dataTypeDefinitionId: $scope.property.dataTypeId,
+					group: $scope.tab.name,
+					localize: true,
+					name: $scope.property.label,
+					propertyEditorAlias: $scope.property.editor
 				}
-			});
-		};
+
+				localizationMigrationResource.postProperty(property).then(function () {
+					$route.reload();
+				});
+			}
+		}
 	};
 }());
