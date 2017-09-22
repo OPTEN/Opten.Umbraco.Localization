045ff4d44037071a1badf89ef8806443a556aacd
Severin d'Heureuse <severin.dheureuse@opten.ch>
2017-09-22 10:59:08 +0200

    implemented GetLocalizedGridHtml to render the grid in the current language and added the extension method GetLocalizedProperty to streamline the implementation to get the localized property in the methods GetLocalizedValue HasLocalizedValue and GetLocalizedGridHtml

---
+++ src/Opten.Umbraco.Localization.Web/Extensions/GridTemplateExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+
+namespace Opten.Umbraco.Localization.Web.Extensions
+{
+	/// <summary>
+	/// Helper to get the grid localized
+	/// </summary>
+	public static class GridTemplateExtensions
+	{
+		/// <summary>
+		/// Gets the localized grid HTML.
+		/// </summary>
+		/// <param name="html">The HTML.</param>
+		/// <param name="contentItem">The content item.</param>
+		/// <param name="propertyAlias">The property alias.</param>
+		/// <param name="framework">The framework.</param>
+		/// <param name="language">The language.</param>
+		/// <returns></returns>
+		public static MvcHtmlString GetLocalizedGridHtml(this HtmlHelper html, IPublishedContent contentItem, string propertyAlias = "bodyText", string framework = "bootstrap3", string language = null)
+		{
+			return GetLocalizedGridHtml(contentItem, html, propertyAlias, framework);
+		}
+
+		/// <summary>
+		/// Gets the localized grid HTML.
+		/// </summary>
+		/// <param name="contentItem">The content item.</param>
+		/// <param name="html">The HTML.</param>
+		/// <param name="propertyAlias">The property alias.</param>
+		/// <param name="framework">The framework.</param>
+		/// <param name="language">The language.</param>
+		/// <returns></returns>
+		/// <exception cref="NullReferenceException">No localized property type found with alias " + propertyAlias</exception>
+		public static MvcHtmlString GetLocalizedGridHtml(this IPublishedContent contentItem, HtmlHelper html, string propertyAlias = "bodyText", string framework = "bootstrap3", string language = null)
+		{
+			Mandate.ParameterNotNullOrEmpty(propertyAlias, "propertyAlias");
+
+			var view = "Grid/" + framework;
+			var prop = contentItem.GetLocalizedProperty(propertyAlias, language, false, true);
+			if (prop == null) throw new NullReferenceException("No localized property type found with alias " + propertyAlias);
+			var model = prop.Value;
+
+			var asString = model as string;
+			if (asString != null && string.IsNullOrEmpty(asString)) return new MvcHtmlString(string.Empty);
+
+			return html.Partial(view, model);
+		}
+	}
+}
