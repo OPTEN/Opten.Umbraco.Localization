f869208de24a40bb6ec7c80d92dfc8cc949f61e9
amorf <aaron.morf@opten.ch>
2019-07-26 12:00:34 +0200

    fix: changed opten.common version

---
+++ src/Opten.Umbraco.Localization.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Opten.Umbraco.Localization.Web.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string ForceEndsWith(this string input, char value)
+		{
+			return input.EndsWith(value.ToString(CultureInfo.InvariantCulture)) ? input : input + value;
+		}
+	}
+}
